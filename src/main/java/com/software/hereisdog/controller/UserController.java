d5512c96c8f663bbc74cba294e49ec1db420a892
yejoo3 <jjoooo315@naver.com>
2025-06-06 08:26:41 +0900

    오류 수정

--- src/main/java/com/software/hereisdog/controller/UserController.java
+++ src/main/java/com/software/hereisdog/controller/UserController.java
@@ -12,23 +12,22 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequestMapping("/user")
-public class User {
+public class UserController {
 
     @Autowired
     private FavoriteService favoriteService;
     @Autowired
     private ReviewService reviewService;
 
-    // ★ 마이페이지: 내 정보, 찜, 리뷰 다 조회
-    @GetMapping("/mypage")
-    public String myPage(Model model, @SessionAttribute("loginUser") Customer customer) {
-        Long userId = customer.getId();
-
-        model.addAttribute("user", customer); // 닉네임/이메일 등 마이페이지 표시
-        model.addAttribute("favList", favoriteService.findByUserId(userId));
-        model.addAttribute("reviewList", reviewService.findByCustomerId(userId));
-        return "user/mypage"; // => /WEB-INF/views/user/mypage.jsp
-    }
+//    @GetMapping("/mypage")
+//    public String myPage(Model model, @SessionAttribute("loginUser") Customer customer) {
+//        Long userId = customer.getId();
+//
+//        model.addAttribute("user", customer); // 닉네임/이메일 등 마이페이지 표시
+//        model.addAttribute("favList", favoriteService.findByUserId(userId));
+//        model.addAttribute("reviewList", reviewService.findByCustomerId(userId));
+//        return "user"; 
+//    }
     
     @GetMapping("/mypage-test")
     public String testMyPage(Model model) {
